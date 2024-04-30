a824053e45bda9a3d7613e130e8774b5efd72292
vim <dan.shumayev@mail.huji.ac.il>
2024-04-30 15:30:03 +0300

    Remove unused packages.

--- src/combine_csvs.py
+++ src/combine_csvs.py
@@ -1,13 +1,6 @@
 import os
 import csv
 import pandas as pd
-import requests
-from urllib.parse import quote_plus
-from bs4 import BeautifulSoup
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import re
 from googleapiclient.discovery import build
 
